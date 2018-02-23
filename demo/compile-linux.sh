131d2c1f67fb4192671fba3fd99c2134bacc67a3
Administrator <Administrator@MS-20170403PFLP>
2018-02-23 14:27:07 +0800

    Add run-test script

--- demo/compile-linux.sh
+++ demo/compile-linux.sh
@@ -1,3 +1,2 @@
 javac -Djava.ext.dirs=lib -cp .:bin -d bin src/org/demo/*.java
 javac -Djava.ext.dirs=lib -cp .:bin -d bin src/tests/*.java
-java -ea -cp .:bin:lib/testng-2.0beta-jdk15.jar com.beust.testng.TestNG src/testng.xml
---
+++ demo/run-test.sh
@@ -0,0 +1,1 @@
+java -ea -cp .:bin:lib/testng-2.0beta-jdk15.jar com.beust.testng.TestNG src/testng.xml
